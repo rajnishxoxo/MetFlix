7464ebe6354a15a0504c3157e7655d67083d3770
Rajnish <srajnish235.rs@gmail.com>
2023-09-28 12:10:10 +0530

    Removing unwanted files

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div className='text-3xl text-red-700'>Hello</div>
-  )
+    return(
+        <div>
+            
+        </div>
+    )
 }
 
 export default App
