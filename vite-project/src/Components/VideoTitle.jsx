f68031b298ab4b28e3672842637b918296b063e8
Rajnish <srajnish235.rs@gmail.com>
2023-10-01 12:09:52 +0530

    Update VideoTitle.jsx

--- vite-project/src/Components/VideoTitle.jsx
+++ vite-project/src/Components/VideoTitle.jsx
@@ -4,7 +4,7 @@ const VideoTitle = ({ title, overview }) => {
 
   return (
     <div className="bg-gradient-to-t from-black">
-    <div className="pt-[48%] lg:mt-[100px]  px-5 absolute aspect-video bg-gradient-to-b from-black w-screen lg:pt-[20%]  lg:px-24 ">
+    <div className="pt-[40%] lg:mt-[100px]  px-5 absolute aspect-video bg-gradient-to-b from-black w-screen lg:pt-[20%]  lg:px-24 ">
       <div>
         <h1 className="text-2xl mx-2 mb-2 text-white lg:text-7xl">{title}</h1>
         <p className="text-sm mx-2 text-white lg:text-3xl lg:font-normal">{overview}</p>
