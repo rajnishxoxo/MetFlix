e57c3f1d8bf80f3e7dbb0103ea94458506a648fd
Rajnish <srajnish235.rs@gmail.com>
2023-10-03 10:55:56 +0530

    installing firebase again

--- vite-project/src/Utils/firebase.js
+++ vite-project/src/Utils/firebase.js
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
+  // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDx2Xqip-kp-yN6IRYTWSJzv003Jsm6aY8",
-  authDomain: "netflixclone-c6ac5.firebaseapp.com",
-  projectId: "netflixclone-c6ac5",
-  storageBucket: "netflixclone-c6ac5.appspot.com",
-  messagingSenderId: "43238373912",
-  appId: "1:43238373912:web:277d3abecfbeabe1c6216d",
-  measurementId: "G-E2WTBBN712"
+  apiKey: "AIzaSyBditUKtd5UPCp_gFifbLENEWkNd3BPTz4",
+  authDomain: "movieapp-a3102.firebaseapp.com",
+  projectId: "movieapp-a3102",
+  storageBucket: "movieapp-a3102.appspot.com",
+  messagingSenderId: "944104800322",
+  appId: "1:944104800322:web:0b3bbd1456fc0e425e6ddb",
+  measurementId: "G-64J245L2N7"
 };
 
 // Initialize Firebase
