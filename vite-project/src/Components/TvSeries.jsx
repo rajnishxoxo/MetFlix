66826c0fdfbbf9e0711488b597aad920c84fe188
Rajnish <srajnish235.rs@gmail.com>
2023-10-05 11:07:06 +0530

    Create TvSeries.jsx

---
+++ vite-project/src/Components/TvSeries.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { API_OPTIONS, IMAGE_PATH } from "../Utils/Contant";
+import { useDispatch } from "react-redux";
+import { addTvShowsDetails } from "../Store/Slices/tvShowsSlice";
+import { useSelector } from "react-redux";
+
+const TvSeries = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  console.log(id);
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/tv/" + id + "?language=en-US",
+      API_OPTIONS
+    );
+
+    const getDetails = await data.json();
+
+    console.log(getDetails);
+
+    const { overview, poster_path, original_name } = getDetails;
+    dispatch(addTvShowsDetails({ overview, poster_path, original_name }));
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+
+  const tvDetails = useSelector((state) => state.tvShows.singleTVShowDetail);
+  if (tvDetails === null) return;
+  const { overview, poster_path, original_name } = tvDetails;
+
+  return (
+    <div className="lg:absolute lg:-z-10 lg:-mt-20">
+      <div className="w-screen absolute -z-20  ">
+        <img
+          className="  w-screen object-contain h-[700px] git pull
+          rounded-xl"
+          src={IMAGE_PATH + poster_path}
+          alt="movie poster"
+        />
+      </div>
+      <div>
+        <div className="bg-gradient-to-t from-black">
+          <div className="pt-[40%] lg:mt-[100px]  px-5 absolute aspect-video bg-gradient-to-b from-black w-screen lg:pt-[20%]  lg:px-24 ">
+            <div>
+              <h1 className="text-2xl mx-2 mb-2 text-white lg:text-7xl">
+                {original_name}
+              </h1>
+              <p className="text-sm mx-2 text-white lg:text-3xl lg:font-normal">
+                {overview}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-gradient-to-t from-black">
+          <div className="pt-[40%] lg:mt-[100px]  px-5 absolute aspect-video bg-gradient-to-b from-black w-screen lg:pt-[20%]  lg:px-24 ">
+            <div>
+              <h1 className="text-2xl mx-2 mb-2 text-white  lg:text-7xl">
+                {original_name}
+              </h1>
+              <p className="text-sm mx-2 text-white lg:text-3xl lg:font-normal">
+                {overview}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {
+        <div className="md:hidden ">
+          <img
+            className=" w-[100%] border-[1px] border-brand-beige mt-8 mx-auto rounded-xl"
+            src={IMAGE_PATH + poster_path}
+            alt="movie poster"
+          />
+        </div>
+      }
+    </div>
+  );
+};
+
+export default TvSeries;
