36140fcd13be281d0467d0b1288fb7f09d810a39
Rajnish <srajnish235.rs@gmail.com>
2023-10-17 19:26:52 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,14 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return defineConfig({
-    define: {
-      'process.env.openAI_keys': JSON.stringify(env.openAI_keys),
-    },
-    plugins: [react()],
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+});
