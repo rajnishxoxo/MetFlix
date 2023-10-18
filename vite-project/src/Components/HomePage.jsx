1fddfcd94ff128847274feb86b044d04b074886c
Rajnish <srajnish235.rs@gmail.com>
2023-10-18 12:11:29 +0530

    Fixing Css

--- vite-project/src/Components/HomePage.jsx
+++ vite-project/src/Components/HomePage.jsx
@@ -87,6 +87,7 @@ const HomePage = () => {
         {showGPT ? (
           <>
             <GptSearchComp />
+           
           </>
         ) : (
           <div>
--- vite-project/src/Components/VideoBackGround.jsx
+++ vite-project/src/Components/VideoBackGround.jsx
@@ -15,7 +15,7 @@ const VideoBackGround = ({ id }) => {
   return (
     <div className="w-screen absolute -z-20  ">
       <iframe
-        className=" w-[400px] mt-5 lg:w-screen lg:h-full aspect-video lg:aspect-video rounded"
+        className=" w-full mt-5 lg:w-screen lg:h-full aspect-video lg:aspect-video rounded"
         height="315"
         src={
           "https://www.youtube.com/embed/" + key + "?autoplay=1&mute=1&rel=0"
